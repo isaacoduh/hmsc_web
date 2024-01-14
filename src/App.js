a0355e654800bc4bea0130a4183316ed50c64cfa
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-01-14 00:07:14 +0000

    create products list page, update product data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/navbar";
+import products_data from './datastore/products_data'
+import Card from "./components/Card";
+import Products from "./products/Products";
+
 
 function App() {
-  return (
+  function filteredData (products) {
+    let filteredProducts = products;
+    return filteredProducts.map(({img, title, star, reviews, prevPrice, newPrice}) => (
+        <Card
+            key={Math.random()}
+            img={img}
+            title={title}
+            star={star}
+            reviews={reviews}
+            prevPrice={prevPrice}
+            newPrice={newPrice}
+        />
+    ));
+  }
 
-    <Navbar/>
+  const result = filteredData(products_data);
+  return (
+    <>
+      <Navbar/>
+      <Products result={result}/>
+    </>
   );
 }
 
